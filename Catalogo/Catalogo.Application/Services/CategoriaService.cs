4218cc8e46f0f4b6557ec4ee1b2c4c02c96eb565
Thiago jose <thiago.ads.silva@gmail.com>
2022-08-24 07:20:32 -0300

    Projeto em .netcore 5 com arquitetura limpa

---
+++ Catalogo/Catalogo.Application/Services/CategoriaService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Catalogo.Application.DTOs;
+using Catalogo.Application.Interfaces;
+using Catalogo.Domain.Entities;
+using Catalogo.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalogo.Application.Services
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private ICategoriaRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoriaService(ICategoriaRepository categoryRepository, 
+            IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoriaDTO>> GetCategorias()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategoriasAsync();
+            return _mapper.Map<IEnumerable<CategoriaDTO>>(categoriesEntity);
+        }
+
+        public async Task<CategoriaDTO> GetById(int? id)
+        {
+            var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+            return _mapper.Map<CategoriaDTO>(categoryEntity);
+        }
+
+        public async Task Add(CategoriaDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Categoria>(categoryDto);
+            await _categoryRepository.CreateAsync(categoryEntity);
+        }
+
+        public async Task Update(CategoriaDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Categoria>(categoryDto);
+            await _categoryRepository.UpdateAsync(categoryEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var categoryEntity = _categoryRepository.GetByIdAsync(id).Result;
+            await _categoryRepository.RemoveAsync(categoryEntity);
+        }
+    }
+}
