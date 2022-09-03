9d98f4dd4fc20ed0e5bbba8e44d87d5c2a6665b7
Thiago jose <thiago.ads.silva@gmail.com>
2022-09-03 06:28:09 -0300

    Atualizando

--- Catalogo/Catalogo.CrossCutting/obj/Release/net5.0/Catalogo.CrossCutting.AssemblyInfo.cs
+++ Catalogo/Catalogo.CrossCutting/obj/Release/net5.0/Catalogo.CrossCutting.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Catalogo.CrossCutting")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API")]
+[assembly: System.Reflection.AssemblyProductAttribute("Catalogo.CrossCutting")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Catalogo.CrossCutting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
